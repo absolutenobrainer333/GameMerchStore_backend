4d62eb042454893c9832c5f304412b28f7519450
Thanh Nguyen <nnnannn710@gmail.com>
2024-08-22 17:22:16 +0700

    add env

--- src/dbcontext/sequelize.js
+++ src/dbcontext/sequelize.js
@@ -1,8 +1,9 @@
 const { Sequelize, DataTypes } = require('sequelize')
+const dotenv = require('dotenv').config();
 
-const sequelize = new Sequelize('db_aac510_gmstore', 'aac510_gmstore', 'dsadsa123', {
-	host: 'mysql8001.site4now.net',
-	dialect: 'mysql',
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+	host: process.env.DATABASE_HOST,
+	dialect: process.env.DATABASE_DIALECT,
 	logging: false,
 	define: {
 		timestamps: false,
--- src/utils/TokenHandler.js
+++ src/utils/TokenHandler.js
@@ -1,31 +1,32 @@
-const jwt = require('jsonwebtoken')
-const { DbContext } = require('../dbcontext/sequelize')
+const jwt = require("jsonwebtoken");
+const { DbContext } = require("../dbcontext/sequelize");
+const dotenv = require('dotenv').config();
 
-const tokensecret = 'gamemerchstoretokensecret'
+const tokensecret = process.env.TOKEN_SECRET;
 
 module.exports = {
-	createToken(user) {
-		const signValue = {
-			id: user.id,
-			role: user.role,
-		}
-		return jwt.sign(signValue, tokensecret, { expiresIn: '1d' })
-	},
+  createToken(user) {
+    const signValue = {
+      id: user.id,
+      role: user.role,
+    };
+    return jwt.sign(signValue, tokensecret, { expiresIn: "1d" });
+  },
 
-	async getUserFromToken(token) {
-		try {
-			const decoded = jwt.verify(token, tokensecret)
-			return await DbContext.User.findByPk(decoded.id)
-		} catch (error) {
-			return null
-		}
-	},
+  async getUserFromToken(token) {
+    try {
+      const decoded = jwt.verify(token, tokensecret);
+      return await DbContext.User.findByPk(decoded.id);
+    } catch (error) {
+      return null;
+    }
+  },
 
-	checkAdminRole(user) {
-		try {
-			return Boolean(user.role)
-		} catch (error) {
-			return false
-		}
-	},
-}
+  checkAdminRole(user) {
+    try {
+      return Boolean(user.role);
+    } catch (error) {
+      return false;
+    }
+  },
+};
