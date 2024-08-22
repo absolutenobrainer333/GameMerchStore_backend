a2594ba19ef32f9b6104cd8716fd0cd255e92712
Thanh Nguyen <nnnannn710@gmail.com>
2024-08-23 05:56:28 +0700

    finishing

--- src/dbcontext/sequelize.js
+++ src/dbcontext/sequelize.js
@@ -1,43 +1,48 @@
-const { Sequelize, DataTypes } = require('sequelize')
-const dotenv = require('dotenv').config();
+const { Sequelize, DataTypes } = require("sequelize");
+const dotenv = require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-	host: process.env.DATABASE_HOST,
-	dialect: process.env.DATABASE_DIALECT,
-	logging: false,
-	define: {
-		timestamps: false,
-		freezeTableName: true,
-	},
-})
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT,
+    logging: false,
+    define: {
+      timestamps: false,
+      freezeTableName: true,
+    },
+  }
+);
 
-const User = require('../entities/User')(sequelize, DataTypes)
-const Category = require('../entities/Category')(sequelize, DataTypes)
-const Product = require('../entities/Product')(sequelize, DataTypes)
-const Order = require('../entities/Order')(sequelize, DataTypes)
-const OrderDetail = require('../entities/OrderDetail')(sequelize, DataTypes)
+const User = require("../entities/User")(sequelize, DataTypes);
+const Category = require("../entities/Category")(sequelize, DataTypes);
+const Product = require("../entities/Product")(sequelize, DataTypes);
+const Order = require("../entities/Order")(sequelize, DataTypes);
+const OrderDetail = require("../entities/OrderDetail")(sequelize, DataTypes);
 
-User.hasMany(Order, { foreignKey: 'userId', as: 'User' })
+User.hasMany(Order, { foreignKey: "userId", as: "User" });
 
-Product.belongsTo(Category, { foreignKey: 'categoryId', as: 'Category' })
-Category.hasMany(Product, { foreignKey: 'categoryId', as: 'Products' })
+Product.belongsTo(Category, { foreignKey: "categoryId", as: "Category" });
+Category.hasMany(Product, { foreignKey: "categoryId", as: "Products" });
 
-Order.hasMany(OrderDetail, { foreignKey: 'orderId', as: 'OrderDetails' })
-Product.hasMany(OrderDetail, { foreignKey: 'productId', as: 'OrderDetails' })
+Order.hasMany(OrderDetail, { foreignKey: "orderId", as: "OrderDetails" });
+Product.hasMany(OrderDetail, { foreignKey: "productId", as: "OrderDetails" });
 
-OrderDetail.belongsTo(Order, { foreignKey: 'orderId', as: 'Order' })
-OrderDetail.belongsTo(Product, { foreignKey: 'productId', as: 'Product' })
+OrderDetail.belongsTo(Order, { foreignKey: "orderId", as: "Order" });
+OrderDetail.belongsTo(Product, { foreignKey: "productId", as: "Product" });
 
 // sequelize.sync({ alter: true })
 
 const DbContext = {
-	User,
-	Category,
-	Product,
-	Order,
-	OrderDetail,
-}
+  User,
+  Category,
+  Product,
+  Order,
+  OrderDetail,
+};
 
-module.exports = { DbContext, sequelize }
+module.exports = { DbContext, sequelize };
 
 // fibis64120@segichen.com
--- src/server.js
+++ src/server.js
@@ -1,31 +1,32 @@
-const express = require('express')
-const cors = require('cors')
-const { sequelize } = require('./dbcontext/sequelize')
-const routerProduct = require('./routes/UserRoute/routerProduct')
-const routerAuth = require('./routes/UserRoute/routerAuth')
-const routerOrder = require('./routes/UserRoute/routerOrder')
-const routerProductManagement = require('./routes/AdminRoute/routerProductManagement')
-const routerGeneral = require('./routes/routerGeneral')
+const express = require("express");
+const cors = require("cors");
+const { sequelize } = require("./dbcontext/sequelize");
+const routerProduct = require("./routes/UserRoute/routerProduct");
+const routerAuth = require("./routes/UserRoute/routerAuth");
+const routerOrder = require("./routes/UserRoute/routerOrder");
+const routerProductManagement = require("./routes/AdminRoute/routerProductManagement");
+const routerGeneral = require("./routes/routerGeneral");
 
-const app = express()
+const app = express();
 
 sequelize.authenticate().then(async () => {
-	app.listen(3001, () => {
-		console.log(`App is running on port 3001`)
-	})
-})
-app.use(cors())
-app.use(express.json())
+  app.listen(3001, () => {
+    console.log(`App is running on port 3001`);
+  });
+});
+
+app.use(cors());
+app.use(express.json());
 
 // General Route
-app.use('/general', routerGeneral)
+app.use("/general", routerGeneral);
 
 // User Route
-app.use('/product', routerProduct)
-app.use('/auth', routerAuth)
-app.use('/order', routerOrder)
+app.use("/product", routerProduct);
+app.use("/auth", routerAuth);
+app.use("/order", routerOrder);
 
 // Admin Route
-app.use('/productManagement', routerProductManagement)
+app.use("/productManagement", routerProductManagement);
 
-app.use('/*', (req, res) => res.status(404).json({ error: 'Page not found' }))
+app.use("/*", (req, res) => res.status(404).json({ error: "Page not found" }));
